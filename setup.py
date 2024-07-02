a6b417120e1da59fc60a729418d39ace03942b58
Chris <christopher.smiet@epfl.ch>
2024-07-02 12:26:42 +0200

    new build

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
+import os
 import setuptools
-from numpy.distutils.core import Extension, setup
-from numpy.distutils.fcompiler import get_default_fcompiler
-
-# from coilpy import __version__
-
-__version__ = "0.4.01"
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-# this grabs the requirements from requirements.txt
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+from setuptools import Extension, setup
 
 # fortran compiler
-compiler = get_default_fcompiler()
+compiler = os.environ.get('FC', 'gfortran')  # replace 'gnu95' with your default compiler
 # set some fortran compiler-dependent flags
 f90flags = []
 if compiler == "gnu95":
     f90flags.append("-ffree-line-length-none")
-elif compiler == "intel" or compiler == "intelem":
+else:
     pass
+
 f90flags.append("-O3")
 
 ext = Extension(
@@ -27,26 +18,10 @@ ext = Extension(
     sources=[
         "coilpy/fortran/biotsavart.f90",
     ],
-    extra_f90_compile_args=f90flags,
+    extra_compile_args=f90flags,
 )
 
 setup(
-    name="coilpy",
-    version=__version__,
-    description="Plotting and data processing tools for plasma and coil",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering",
-    ],
-    url="https://github.com/zhucaoxiang/CoilPy",
-    author="Caoxiang Zhu",
-    author_email="caoxiangzhu@gmail.com",
-    license="GNU 3.0",
     packages=setuptools.find_packages(),
     ext_modules=[ext],
-    install_requires=REQUIREMENTS,
 )
